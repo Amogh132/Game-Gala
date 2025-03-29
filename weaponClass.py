6f72674577cbee5ad40076328768e48493632c63
outofmoment <imlc7lc@gmail.com>
2025-03-29 12:46:28 -0700

    Update weaponClass.py

--- weaponClass.py
+++ weaponClass.py
@@ -1,1 +1,14 @@
 #make here a class that creates weapons their drop chance and the stats
+class weapons:
+    def __init__(self,name,damage,dropchance):
+        self.name=name
+        self.damage=damage
+        self.damage=damage
+        self.dropchance=dropchance
+
+weapontypes = [
+  weapons("beam saber",20,.45),
+  weapons("plasma rifle",35,.15),
+  weapons("energy shield",15,.65),
+  weapons("light grenade",80,.03)  
+]
